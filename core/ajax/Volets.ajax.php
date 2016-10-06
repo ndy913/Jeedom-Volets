54f8740f790c747cf2b9ce2080a56aae2c9d1691
Halbout Michael <michaelhalbout@gmail.com>
2016-10-06 13:43:32 +0200

    Update Volets.ajax.php

--- core/ajax/Volets.ajax.php
+++ core/ajax/Volets.ajax.php
@@ -1,43 +1,26 @@
 <?php
-
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-
 try {
-    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-    include_file('core', 'authentification', 'php');
+	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+	include_file('core', 'authentification', 'php');
 
-    if (!isConnect('admin')) {
-        throw new Exception(__('401 - Accès non autorisé', __FILE__));
-    }
-    if (init('action') == 'getInformation') {
-        $result=array();
-        $heliotrope=eqLogic::byId(init('heliotrope'));
-        if(is_object($heliotrope)){
-           $result['heliotrope']=utils::o2a($heliotrope);
-        }
-        $equipement=eqLogic::byId(init('id'));
-        if(is_object($equipement)){
-           $result['Volets']=utils::o2a($equipement->getCmds());
-        }
+	if (!isConnect('admin')) {
+		throw new Exception(__('401 - Accès non autorisé', __FILE__));
+	}
+	if (init('action') == 'getInformation') {
+		$result=array();
+		$heliotrope=eqLogic::byId(init('heliotrope'));
+		if(is_object($heliotrope)){
+			//$result['heliotrope']=utils::o2a($heliotrope);
+		}
+		$equipement=eqLogic::byId(init('id'));
+		if(is_object($equipement)){
+			$result['Volets']=utils::o2a($equipement->getCmds());
+		}
 		ajax::success($result);
 	}
-    throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
-    /*     * *********Catch exeption*************** */
+	throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+	/*     * *********Catch exeption*************** */
 } catch (Exception $e) {
-    ajax::error(displayExeption($e), $e->getCode());
+	ajax::error(displayExeption($e), $e->getCode());
 }
 ?>

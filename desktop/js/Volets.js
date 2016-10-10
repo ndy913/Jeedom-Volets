9d2ba04fa3c68189186a69cd593bc6f09b747471
Halbout Michael <michaelhalbout@gmail.com>
2016-10-10 10:20:23 +0200

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -61,29 +61,6 @@ function addCmdToTable(_cmd) {
 	if (!isset(_cmd)) {
 	var _cmd = {configuration: {}};
 	}
-	
-	var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
-	tr += '<td class="name">';
-	tr += '<input class="cmdAttr form-control input-sm" data-l1key="id" style="display : none;">';
-	tr += '<input class="cmdAttr form-control input-sm" data-l1key="logicalId">';
-	tr += '<input class="cmdAttr form-control input-sm" data-l1key="name">   ';
-	tr += '</td>';
-
-	tr += '<td class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType();
-	tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span></td>';
-	tr += '<td>';
-	tr += '<span><input type="checkbox" data-size="mini" data-label-text="{{Historiser}}" class="cmdAttr bootstrapSwitch" data-l1key="isHistorized" /></span> ';
-	tr += '</td>';
-	tr += '<td>';
-	if (is_numeric(_cmd.id)) {
-		tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible" data-action="configure"><i class="fa fa-cogs"></i></a> ';
-		tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
-	}
-	tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
-	tr += '</tr>';
-	$('#table_cmd tbody').append(tr);
-	$('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
-	$('#table_cmd tbody tr:last').find('.cmdAttr[data-l1key=logicalId]').val(JSON.stringify(Coordinates));
 	jeedom.cmd.changeType($('#table_cmd tbody tr:last'), init(_cmd.subType));
 	//if (typeof(_cmd.logicalId) !== 'undefined' && _cmd.logicalId != "") 
 	//	Coordinates = _cmd.logicalId; 
@@ -91,7 +68,6 @@ function addCmdToTable(_cmd) {
 		Coordinates.Position= new Object();
 		Coordinates.Position.lat=Coordinates.Center.lat;
 		Coordinates.Position.lng=Coordinates.Center.lng+ (1 / 3600);
-		$('.cmd[data-cmd_id=' + init(_cmd.id) + ']').find('.cmdAttr[data-l1key=logicalId]').val(myLatLng);
 	//}
 	var position=new google.maps.Marker({
 		position: Coordinates.Center,
@@ -116,4 +92,27 @@ function addCmdToTable(_cmd) {
 		TracePolyLigne();
 		$('.cmd[data-cmd_id=' + init(_cmd.id) + ']').find('.cmdAttr[data-l1key=logicalId]').val(JSON.stringify(Coordinates));
 	});
+	var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
+	tr += '<td class="name">';
+	tr += '<input class="cmdAttr form-control input-sm" data-l1key="id" style="display : none;">';
+	tr += '<input class="cmdAttr form-control input-sm" data-l1key="logicalId">';
+	tr += '<input class="cmdAttr form-control input-sm" data-l1key="name">   ';
+	tr += '</td>';
+
+	tr += '<td class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType();
+	tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span></td>';
+	tr += '<td>';
+	tr += '<span><input type="checkbox" data-size="mini" data-label-text="{{Historiser}}" class="cmdAttr bootstrapSwitch" data-l1key="isHistorized" /></span> ';
+	tr += '</td>';
+	tr += '<td>';
+	if (is_numeric(_cmd.id)) {
+		tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible" data-action="configure"><i class="fa fa-cogs"></i></a> ';
+		tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
+	}
+	tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
+	tr += '</tr>';
+	$('#table_cmd tbody').append(tr);
+	$('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
+	$('#table_cmd tbody tr:last').find('.cmdAttr[data-l1key=logicalId]').val(JSON.stringify(Coordinates));
+	
 }

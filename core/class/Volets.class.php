c4ffabda68731b7a95ab4fc1f926a7d112685d9b
Halbout Michael <michaelhalbout@gmail.com>
2016-10-10 11:46:47 +0200

    Création de la logique d'evaluation

--- core/class/Volets.class.php
+++ core/class/Volets.class.php
@@ -2,16 +2,6 @@
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
 class Volets extends eqLogic {
-	protected function getAngle($latitudeOrigine,$longitudeOrigne, $latitudeDest,$longitudeDest) {
-		/*$longDelta = $longitudeDest - $longitudeOrigne;
-		$y = sin($longDelta) * cos($latitudeDest);
-		$x = cos($latitudeOrigine)*sin($latitudeDest) - sin($latitudeOrigine)*cos($latitudeDest)*.cos($longDelta);
-		$angle = rad2deg(atan2(y, x));
-		while ($angle < 0) {
-			$angle += 360;
-		}
-		return  $angle;*/
-	}
   	public function preUpdate() {
     	}  
    	public function preInsert() {
@@ -36,7 +26,46 @@ class Volets extends eqLogic {
 }
 
 class VoletsCmd extends cmd {
+	public function getAngle($latitudeOrigine,$longitudeOrigne, $latitudeDest,$longitudeDest) {
+		/*double longDelta = longitudeDest - longitudeOrigne;
+		double y = Math.sin(longDelta) * Math.cos(latitudeDest);
+		double x = Math.cos(latitudeOrigine)*Math.sin(latitudeDest) -Math.sin(latitudeOrigine)*Math.cos(latitudeDest)*Math.cos(longDelta);
+		double angle = Math.toDegrees(Math.atan2(y, x));
+		while (angle < 0) {
+			angle += 360;
+		}
+		return (float) angle % 360;*/
+		$longDelta = $longitudeDest - $longitudeOrigne;
+		$y = sin($longDelta) * cos($latitudeDest);
+		$x = cos($latitudeOrigine)*sin($latitudeDest) - sin($latitudeOrigine)*cos($latitudeDest)*cos($longDelta);
+		$angle = rad2deg(tan(y, x));
+		while ($angle < 0) {
+			$angle += 360;
+		}
+		return  $angle;
+	}
     public function execute($_options = null) {
+	    
+		//Rechercher position du soleil => heliotrope
+		$heliotrope=eqlogic::byId($this->getEqLogic()->getConfiguration('heliotrope'));
+		if(is_object($heliotrope)){
+			$Azimuth=$heliotrope->getCmd(null,'');
+			//Calculer de l'angle de ma zone
+			$Coord=json_decode($this->getLogicalId(),true);
+			$Angle=$this->getAngle($Coord['Center']['lat'],
+					       $Coord['Center']['lng'],
+					       $Coord['Position']['lat'],
+					       $Coord['Position']['lng']);
+			//si l'Azimuth est compris entre mon angle et 180° on est dans la fenetre
+			if($Azimuth>$Angle&&$Azimuth>$Angle-180){
+				$value=true;
+			}
+			else
+				$value=false;
+			$this->setCollectDate(date('Y-m-d H:i:s'));
+			$this->save();
+			$this->event($value);
+		}
     }
 }
 ?>

70b8a70b979d6b7d456bfe41ba5c7911167668d7
Halbout Michael <michaelhalbout@gmail.com>
2016-10-11 10:10:38 +0200

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -64,7 +64,7 @@ function saveEqLogic(_eqLogic) {
 	if (typeof( _eqLogic.cmd) !== 'undefined') {
 		for(var index in  _eqLogic.cmd) { 
 			
-		_eqLogic.cmd[index].configration.action= new Object();
+		_eqLogic.cmd[index].configration.action= new Array();
 			 _eqLogic.cmd[index].configration.action.in=$('#tab_' +init(_eqLogic.cmd[index].id+' .ActionIn')).getValues('.expressionAttr');
 			 _eqLogic.cmd[index].configration.action.out=$('#tab_' +init(_eqLogic.cmd[index].id+' .ActionOut')).getValues('.expressionAttr');
 		}

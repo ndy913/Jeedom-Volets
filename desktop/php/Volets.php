950dc14c4b1a8559ea29a7cdf3980b899de62bee
Halbout Michael <michaelhalbout@gmail.com>
2016-10-18 09:50:42 +0200

    Update Volets.php

--- desktop/php/Volets.php
+++ desktop/php/Volets.php
@@ -52,11 +52,10 @@ $eqLogics = eqLogic::byType('Volets');
 		<form class="form-horizontal">
 			<fieldset>
 				<legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Configuration des zones et actions}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-			
-					<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqFW26gzrAcgO7C2mKNr2A9Y76rd8pSQ8"></script>
-					<div id="map" style="width:75%;height:75%"></div>
 			</fieldset> 
-		</form>
+		</form>	
+		<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqFW26gzrAcgO7C2mKNr2A9Y76rd8pSQ8"></script>
+		<div id="map" style="width: 50%;height: 50%;"></div>
 		<div class="row" style="padding-left:25px;">
 			<ul class="nav nav-tabs" id="tab_zones">	
 				<li><a class="btn cmdAction" data-action="add"><i class="fa fa-plus-circle"></i>{{Ajouter}}</a></li>

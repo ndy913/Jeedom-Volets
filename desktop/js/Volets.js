94c39a8c4a9be3633e40d78669aa92dc95083b8b
Halbout Michael <michaelhalbout@gmail.com>
2016-11-14 08:17:39 +0100

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -36,6 +36,18 @@ $('body').on('change','.eqLogicAttr[data-l1key=configuration][data-l2key=heliotr
 		}
 	});
 });
+$('body').on('change','.eqLogicAttr[data-l1key=configuration][data-l2key=TypeGestion]',function(){
+	switch($(this).val()){
+		case 'DayNight':
+			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisDay]').parent().parent().show();
+			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisNight]').parent().parent().show();
+		break;
+		case 'Helioptrope':
+			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisDay]').parent().parent().hide();
+			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisNight]').parent().parent().hide();
+		break;
+	}
+});
 function getAngle(Coordinates) {
 		var longDelta = Coordinates[1].lng - Coordinates[0].lng;
 		var y = Math.sin(longDelta) * Math.cos(Coordinates[1].lat);

6d9064c0ad159b983101cea00a2438e081fbf64b
Halbout Michael <michaelhalbout@gmail.com>
2017-01-25 09:01:21 +0100

    Update Volets.php

--- desktop/php/Volets.php
+++ desktop/php/Volets.php
@@ -4,7 +4,7 @@ throw new Exception('{{401 - Accès non autorisé}}');
 }
 sendVarToJS('eqType', 'Volets');
 $eqLogics = eqLogic::byType('Volets');
-include_file('desktop', 'OpenLayers.js', 'js', 'Volets');
+include_file('desktop', 'OpenLayers', 'js', 'Volets');
 ?>
 <!--script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqFW26gzrAcgO7C2mKNr2A9Y76rd8pSQ8&libraries=geometry"></script-->
 <div class="row row-overflow">

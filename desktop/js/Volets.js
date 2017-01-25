f9e28549f07adc651edbf3af420237e315b8f2e7
Halbout Michael <michaelhalbout@gmail.com>
2017-01-25 11:51:36 +0100

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -29,7 +29,7 @@ $('body').on('change','.eqLogicAttr[data-l1key=configuration][data-l2key=heliotr
 				CentreLatLng.lng=parseFloat(center[1]);
 				// création de la carte
 				$('#MyMap').show();
-				//map = new OpenLayers.Map("MyMap");
+				map = new OpenLayers.Map("MyMap");
     				//map.addLayer(new OpenLayers.Layer.OSM());
 				//map.setCenter (CentreLatLng, 20);
 				/*map = new google.maps.Map( document.getElementById('MyMap'),{

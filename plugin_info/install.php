ee166abf61a5b51e9577b9e4665510b31336ff2f
Halbout Michael <michaelhalbout@gmail.com>
2017-05-09 14:53:34 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -3,6 +3,8 @@ function Volets_update(){
   foreach(eqLogic::byType('Volets') as $eqLogic){
     $Armed=$eqLogic->getCmd(null,"arme");
     $Armed->remove();
+    $Released=$eqLogic->getCmd(null,"disable");
+    $Released->remove();
     $eqLogic->save();
   }
 }

449b15ae6e1954d9317d73ad8454e783c0fb869a
Halbout Michael <michaelhalbout@gmail.com>
2017-05-15 08:24:47 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -1,13 +1,29 @@
 <?php
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+function Volets_install(){
+	log::add('Volets','debug','Lancement du script de mise a jours'); 
+	foreach(eqLogic::byType('Volets') as $eqLogic){
+		$Armed=$eqLogic->getCmd('',"arme");
+		if(is_object($Armed))
+			$Armed->remove();
+		$Released=$eqLogic->getCmd('',"disable");
+		if(is_object($Released))
+			$Released->remove();
+		$eqLogic->save();
+	}
+	log::add('Volets','debug','Fin du script de mise a jours'); 
+}
 function Volets_update(){
 	log::add('Volets','debug','Lancement du script de mise a jours'); 
 	foreach(eqLogic::byType('Volets') as $eqLogic){
 		$Armed=$eqLogic->getCmd('',"arme");
-		$Armed->remove();
+		if(is_object($Armed))
+			$Armed->remove();
 		$Released=$eqLogic->getCmd('',"disable");
-		$Released->remove();
+		if(is_object($Released))
+			$Released->remove();
 		$eqLogic->save();
 	}
+	log::add('Volets','debug','Fin du script de mise a jours'); 
 }
 ?>

f27f947e9eb04ecb9c9dc1c097315b96160a54d5
Halbout Michael <michaelhalbout@gmail.com>
2017-05-17 15:28:57 +0200

    Update Volets.php

--- desktop/php/Volets.php
+++ desktop/php/Volets.php
@@ -8,8 +8,7 @@ $eqLogics = eqLogic::byType('Volets');
 ?>
 <div class="row row-overflow">
 	<link rel="stylesheet" href="https://openlayers.org/en/v4.1.1/css/ol.css" type="text/css">
-	    <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL"></script>
-	    <script src="https://openlayers.org/en/v4.1.1/build/ol.js"></script>
+	<script src="https://openlayers.org/en/v4.1.1/build/ol.js"></script>
 	<div class="col-lg-2">
 		<div class="bs-sidebar">
 			<ul id="ul_eqLogic" class="nav nav-list bs-sidenav">

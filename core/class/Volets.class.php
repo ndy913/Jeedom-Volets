44616c32cdec9f3199e8ab0faa0dba021dbb2a8e
Halbout Michael <michaelhalbout@gmail.com>
2017-06-01 08:27:30 +0200

    Update Volets.class.php

--- core/class/Volets.class.php
+++ core/class/Volets.class.php
@@ -37,13 +37,13 @@ class Volets extends eqLogic {
 	}
 	public static function deamon_stop() {	
 		foreach(eqLogic::byType('Volets') as $Volet){
-			$listener = listener::byClassAndFunction('Volets', 'pull', array('Volets_id' => intval($Volet->getId()));
+			$listener = listener::byClassAndFunction('Volets', 'pull', array('Volets_id' => $Volet->getId()));
 			if (is_object($listener))
 				$listener->remove();
-			$cron = cron::byClassAndFunction('Volets', 'ActionJour', array('Volets_id' => intval($Volet->getId()));
+			$cron = cron::byClassAndFunction('Volets', 'ActionJour', array('Volets_id' => $Volet->getId());
 			if (is_object($cron)) 	
 				$cron->remove();
-			$cron = cron::byClassAndFunction('Volets', 'ActionNuit', array('Volets_id' => intval($Volet->getId()));
+			$cron = cron::byClassAndFunction('Volets', 'ActionNuit', array('Volets_id' => $Volet->getId());
 			if (is_object($cron)) 	
 				$cron->remove();
 		}

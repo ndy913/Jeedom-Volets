d9ba384a89e7458309be5174052c0b2db2f682a2
Halbout Michael <michaelhalbout@gmail.com>
2017-06-07 08:03:52 +0200

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -450,7 +450,7 @@ $('body').on('click','.ActionAttr[data-action=remove]', function () {
 	$(this).closest('.ActionGroup').remove();
 });
 $("body").on('click', ".listAction", function() {
-	var el = $(this).closest('.form-group').find('.expressionAttr[data-l1key=cmd]');
+	var el = $(this).closest('td').find('.expressionAttr[data-l1key=cmd]');
 	jeedom.getSelectActionModal({}, function (result) {
 		el.value(result.human);
 		jeedom.cmd.displayActionOption(el.value(), '', function (html) {
@@ -459,7 +459,7 @@ $("body").on('click', ".listAction", function() {
 	});
 }); 
 $("body").on('click', ".listCmdAction", function() {
-	var el = $(this).closest('.form-group').find('.expressionAttr[data-l1key=cmd]');
+	var el = $(this).closest('td').find('.expressionAttr[data-l1key=cmd]');
 	jeedom.cmd.getSelectModal({cmd: {type: 'action'}}, function (result) {
 		el.value(result.human);
 		jeedom.cmd.displayActionOption(el.value(), '', function (html) {

51cef7ad4fca2ccbbc5ab07b81c8169aad3fdba0
Halbout Michael <michaelhalbout@gmail.com>
2017-06-26 08:43:53 +0200

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -541,3 +541,9 @@ function getAngle(Origine, Destination) {
 	}
 	return angle % 360;
 }
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};

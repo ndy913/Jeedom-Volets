56806c405df862b39fdcc811c937435dff21629f
Halbout Michael <michaelhalbout@gmail.com>
2017-07-26 09:49:13 +0200

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -261,11 +261,9 @@ function addAction(_action,  _el) {
 				.append($('<a class="btn btn-success btn-sm listAction" title="Sélectionner un mot-clé">')
 					.append($('<i class="fa fa-tasks">')))
 				.append($('<a class="btn btn-success btn-sm listCmdAction data-type="action"">')
-					.append($('<i class="fa fa-list-alt">'))))));
-	tr.append($('<td>')
+					.append($('<i class="fa fa-list-alt">')))))
 	       .append($(jeedom.cmd.displayActionOption(init(_action.cmd, ''), _action.options))));
 	tr.append(addParameters());
-
         _el.append(tr);
         _el.find('tr:last').setValues(_action, '.expressionAttr');
   

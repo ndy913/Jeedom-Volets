b3b2960cea82bef03c970931309aeb5ea520ee66
Halbout Michael <michaelhalbout@gmail.com>
2017-09-04 08:55:57 +0200

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -453,11 +453,10 @@ function addCmdToTable(_cmd) {
 	var tr =$('<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">');
 	tr.append($('<td>')
 		.append($('<input type="hidden" class="cmdAttr form-control input-sm" data-l1key="id">'))
+		.append($('<input type="hidden" class="cmdAttr form-control input-sm" data-l1key="type">'))
+		.append($('<input type="hidden" class="cmdAttr form-control input-sm" data-l1key="subType">'))
 		.append($('<input class="cmdAttr form-control input-sm" data-l1key="name" value="' + init(_cmd.name) + '" placeholder="{{Name}}" title="Name">')));
 	var parmetre=$('<td>');	
-	parmetre.append($('<span class="type" type="' + init(_cmd.type) + '">')
-			.append(jeedom.cmd.availableType()));
-	parmetre.append($('<span class="subType" subType="'+init(_cmd.subType)+'">'));
 	if (is_numeric(_cmd.id)) {
 		parmetre.append($('<a class="btn btn-default btn-xs cmdAction" data-action="test">')
 			.append($('<i class="fa fa-rss">')

ee724acdc48fccd87ffff696d4966531282f5319
Halbout Michael <michaelhalbout@gmail.com>
2017-09-05 12:48:25 +0200

    Update Volets.ajax.php

--- core/ajax/Volets.ajax.php
+++ core/ajax/Volets.ajax.php
@@ -11,7 +11,8 @@ try {
 		$heliotrope=eqLogic::byId(init('heliotrope'));
 		if(is_object($heliotrope)){
 			$result['heliotrope']=utils::o2a($heliotrope->getCmd());
-			$geoloc=eqLogic::byId($heliotrope->getConfiguration('geoloc'));
+			//$geoloc=eqLogic::byId($heliotrope->getConfiguration('geoloc'));
+			$geoloc = geotravCmd::byEqLogicIdAndLogicalId($heliotrope->getConfiguration('geoloc'),'location:coordinate');
 			if(is_object($geoloc)){
 				$result['geoloc']=utils::o2a($geoloc);
 			}

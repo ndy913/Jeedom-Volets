e393e80c77882ce938b4f534faae66238cfe18ad
Halbout Michael <michaelhalbout@gmail.com>
2018-01-19 15:56:34 +0100

    Update Volets.class.php

--- core/class/Volets.class.php
+++ core/class/Volets.class.php
@@ -190,7 +190,6 @@ class Volets extends eqLogic {
 			$Saison=$Volet->getSaison();
 			$Evenement=$Volet->checkCondition('open',$Saison,'Jour');
 			if( $Evenement!= false){
-				$Volet->checkAndUpdateCmd('gestion','Jour');
 				if(!$Volet->CheckOtherGestion('Jour'))
 					return;
 				$Volet->CheckActions('Jour',$Evenement,$Saison);

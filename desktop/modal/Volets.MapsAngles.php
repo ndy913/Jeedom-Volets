6f101eec000706873dd7f3045bdc02ef26755e2e
Halbout Michael <michaelhalbout@gmail.com>
2018-01-26 15:30:56 +0100

    Update Volets.MapsAngles.php

--- desktop/modal/Volets.MapsAngles.php
+++ desktop/modal/Volets.MapsAngles.php
@@ -1,3 +1,9 @@
+<?php
+	if (!isConnect('admin')) {
+		throw new Exception('{{401 - Accès non autorisé}}');
+	}
+	sendVarToJS('BingAPIKey', config::byKey('BingAPIKey','Volets'));
+?>
 <select id="layer-select">
   <option value="Aerial">Aerial</option>
   <option value="AerialWithLabels" selected>Aerial with labels</option>
@@ -22,73 +28,68 @@ var styles = [
 var DroitLatLng=new Object();
 var CentreLatLng=new Object();
 var GaucheLatLng=new Object();
+$(function() {
 $('#MyMap').html('');
-	$.ajax({
-		type: 'POST',            
-		async: false,
-		url: 'plugins/Volets/core/ajax/Volets.ajax.php',
-		data:{
-			action: 'getInformation',
-			heliotrope:$(this).val(),
-		},
-		dataType: 'json',
-		global: false,
-		error: function(request, status, error) {},
-		success: function(data) {
-			if (!data.result)
-				$('#div_alert').showAlert({message: 'Aucun message reçu', level: 'error'});
-			if (typeof(data.result.geoloc) !== 'undefined') {
-				//var center=data.result.geoloc.configuration.coordinate.split(",");
-				var center=data.result.geoloc.split(",");
-				CentreLatLng.lat=parseFloat(center[0]);
-				CentreLatLng.lng=parseFloat(center[1]);
-				var view = new ol.View({
-					center: ol.proj.fromLonLat([CentreLatLng.lng,CentreLatLng.lat]),
-					zoom: 10
-    				});
-				if(BingAPIKey != ""){
-						for (i = 0, ii = styles.length; i < ii; ++i) {
-						layers.push(new ol.layer.Tile({
-							visible: false,
-							preload: Infinity,
-							source: new ol.source.BingMaps({
-								key: BingAPIKey,
-								imagerySet: styles[i]
-							})
-						}));
+	if($('.eqLogicAttr[data-l1key=configuration][data-l2key=heliotrope]').val() !=''){
+		$.ajax({
+			type: 'POST',            
+			async: false,
+			url: 'plugins/Volets/core/ajax/Volets.ajax.php',
+			data:{
+				action: 'getInformation',
+				heliotrope:$('.eqLogicAttr[data-l1key=configuration][data-l2key=heliotrope]').val()
+			},
+			dataType: 'json',
+			global: false,
+			error: function(request, status, error) {},
+			success: function(data) {
+				if (!data.result)
+					$('#div_alert').showAlert({message: 'Aucun message reçu', level: 'error'});
+				if (typeof(data.result.geoloc) !== 'undefined') {
+					//var center=data.result.geoloc.configuration.coordinate.split(",");
+					var center=data.result.geoloc.split(",");
+					CentreLatLng.lat=parseFloat(center[0]);
+					CentreLatLng.lng=parseFloat(center[1]);
+					var view = new ol.View({
+						center: ol.proj.fromLonLat([CentreLatLng.lng,CentreLatLng.lat]),
+						zoom: 10
+						});
+					if(BingAPIKey != ""){
+							for (i = 0, ii = styles.length; i < ii; ++i) {
+							layers.push(new ol.layer.Tile({
+								visible: false,
+								preload: Infinity,
+								source: new ol.source.BingMaps({
+									key: BingAPIKey,
+									imagerySet: styles[i]
+								})
+							}));
+						}
+						map = new ol.Map({
+							layers: layers,
+							loadTilesWhileInteracting: true,
+							target: 'MyMap',
+							view: view
+						});
+						layers[3].setVisible(styles[3]);
+					}else{
+						$('#layer-select').hide();
+						map =new ol.Map({
+							view: view,
+							layers: [
+								new ol.layer.Tile({
+									source: new ol.source.OSM()
+								})
+							],
+							target: 'MyMap'
+						});
 					}
-					map = new ol.Map({
-						layers: layers,
-						loadTilesWhileInteracting: true,
-						target: 'MyMap',
-						view: view
-					});
-					layers[3].setVisible(styles[3]);
-				}else{
-					$('#layer-select').hide();
-					map =new ol.Map({
-						view: view,
-						layers: [
-							new ol.layer.Tile({
-								source: new ol.source.OSM()
-							})
-						],
-						target: 'MyMap'
-					});
+					TraceMapZone()
 				}
-				/*var geolocation = new ol.Geolocation({projection: view.getProjection()});
-        			geolocation.setTracking(true);
-				geolocation.on('change', function() {
-					//geolocation.getAccuracy() + ' [m]';
-					alert(geolocation.getAltitude() + ' [m]');
-					//geolocation.getAltitudeAccuracy() + ' [m]';
-					//geolocation.getHeading() + ' [rad]';
-					//geolocation.getSpeed() + ' [m/s]';
-				});*/
 			}
-		}
-	});
-  
+		});
+	} 
+});
 $('body').on('change','#layer-select',function(){
         var style = $(this).val();
         for (var i = 0, ii = layers.length; i < ii; ++i) {
@@ -97,29 +98,15 @@ $('body').on('change','#layer-select',function(){
 });
 
 function TraceMapZone(_zone){
-	DroitLatLng.lat=CentreLatLng.lat;
-	DroitLatLng.lng=CentreLatLng.lng- (1 / 3600);
-	GaucheLatLng.lat=CentreLatLng.lat;
-	GaucheLatLng.lng=CentreLatLng.lng+ (1 / 3600);
-	if (typeof(_zone.configuration.Droite) !== 'undefined' && _zone.configuration.Droite != "") {
-		if (typeof(_zone.configuration.Droite.lat) !== 'undefined' && _zone.configuration.Droite.lat != "" ) 
-			DroitLatLng.lat=parseFloat(_zone.configuration.Droite.lat);
-		if (typeof(_zone.configuration.Droite.lng) !== 'undefined' && _zone.configuration.Droite.lng != "" ) 
-			DroitLatLng.lng=parseFloat(_zone.configuration.Droite.lng);
-	}
-	if (typeof(_zone.configuration.Gauche) !== 'undefined' && _zone.configuration.Gauche != "") {
-		if (typeof(_zone.configuration.Gauche.lat) !== 'undefined' && _zone.configuration.Gauche.lat != "" ) 
-			GaucheLatLng.lat=parseFloat(_zone.configuration.Gauche.lat);
-		if (typeof(_zone.configuration.Gauche.lng) !== 'undefined' && _zone.configuration.Gauche.lng != "" ) 
-			GaucheLatLng.lng=parseFloat(_zone.configuration.Gauche.lng);
-	}
-	if (typeof(_zone.configuration.Centre) !== 'undefined' && _zone.configuration.Centre != "") {
-		if (typeof(_zone.configuration.Centre.lat) !== 'undefined' && _zone.configuration.Centre.lat != "" ) 
-			CentreLatLng.lat=parseFloat(_zone.configuration.Centre.lat);
-		if (typeof(_zone.configuration.Centre.lng) !== 'undefined' && _zone.configuration.Centre.lng != "" )
-			CentreLatLng.lng=parseFloat(_zone.configuration.Centre.lng);
-	}
-	
+	DroitLatLng=$.parseJSON($('.eqLogicAttr[data-l1key=configuration][data-l2key=Droite]').val());
+	DroitLatLng.lat=parseFloat(DroitLatLng.lat);
+	DroitLatLng.lng=parseFloat(DroitLatLng.lng);
+	GaucheLatLng=$.parseJSON($('.eqLogicAttr[data-l1key=configuration][data-l2key=Gauche]').val());
+	GaucheLatLng.lat=parseFloat(GaucheLatLng.lat);
+	GaucheLatLng.lng=parseFloat(GaucheLatLng.lng);
+	CentreLatLng=$.parseJSON($('.eqLogicAttr[data-l1key=configuration][data-l2key=Centre]').val());
+	CentreLatLng.lat=parseFloat(CentreLatLng.lat);
+	CentreLatLng.lng=parseFloat(CentreLatLng.lng);
 	$('.eqLogicAttr[data-l1key=configuration][data-l2key=AngleDroite]').val(getAngle(CentreLatLng,DroitLatLng));
 	$('.eqLogicAttr[data-l1key=configuration][data-l2key=AngleGauche]').val(getAngle(CentreLatLng,GaucheLatLng));
 	var features = [];
@@ -144,7 +131,8 @@ function TraceMapZone(_zone){
 				}),
       			}),
 			text: new ol.style.Text({
-				text: _zone.name + " - Droite vue extérieure",
+				//text: _zone.name + " - Droite vue extérieure",
+				text: $('.eqLogicAttr[data-l1key=name]').val() + " - Droite vue extérieure",
 				offsetY: -25,
 				fill: new ol.style.Fill({
 					color: '#fff'
@@ -190,7 +178,7 @@ function TraceMapZone(_zone){
 	features.push(Centre);
 	var Gauche = new ol.Feature({
 		geometry: new ol.geom.Point(ol.proj.transform([GaucheLatLng.lng,GaucheLatLng.lat], 'EPSG:4326', 'EPSG:3857')),
-		style: new ol.style.Style({text:new ol.style.Text({text: _zone.name + " - Gauche vue extérieure"})})
+		style: new ol.style.Style({text:new ol.style.Text({text: $('.eqLogicAttr[data-l1key=name]').val() + " - Gauche vue extérieure"})})
 	});
 	Gauche.setStyle([
 		new ol.style.Style({  
@@ -204,7 +192,7 @@ function TraceMapZone(_zone){
 				}),
       			}),
 			text: new ol.style.Text({
-				text: _zone.name + " - Gauche vue extérieure",
+				text: $('.eqLogicAttr[data-l1key=name]').val() + " - Gauche vue extérieure",
 				offsetY: -25,
 				fill: new ol.style.Fill({
 					color: '#fff'
@@ -240,24 +228,24 @@ function TraceMapZone(_zone){
 	}
 }
 function getAngle(Origine, Destination) { 
-	var rlongitudeOrigine = Math.radians(Origine.lng); 
-	var rlatitudeOrigine = Math.radians(Origine.lat); 
-	var rlongitudeDest = Math.radians(Destination.lng); 
-	var rlatitudeDest = Math.radians(Destination.lat); 
+	var rlongitudeOrigine = Deg2Rad(Origine.lng); 
+	var rlatitudeOrigine = Deg2Rad(Origine.lat); 
+	var rlongitudeDest = Deg2Rad(Destination.lng); 
+	var rlatitudeDest = Deg2Rad(Destination.lat); 
 	var longDelta = rlongitudeDest - rlongitudeOrigine; 
 	var y = Math.sin(longDelta) * Math.cos(rlatitudeDest); 
 	var x = (Math.cos(rlatitudeOrigine)*Math.sin(rlatitudeDest)) - (Math.sin(rlatitudeOrigine)*Math.cos(rlatitudeDest)*Math.cos(longDelta)); 
-	var angle = Math.degrees(Math.atan2(y, x)); 
+	var angle = Rad2Deg(Math.atan2(y, x)); 
 	if (angle < 0) { 
 
 		angle += 360; 
 	}
 	return Math.round(angle % 360);
 }
-Math.radians = function(degrees) {
+Deg2Rad = function(degrees) {
   return degrees * Math.PI / 180;
 };
-Math.degrees = function(radians) {
+Rad2Deg = function(radians) {
   return radians * 180 / Math.PI;
 };
 

a852790f2b07887d2ba7dce76bb61a1d2d1bab49
Halbout Michael <michaelhalbout@gmail.com>
2018-04-18 09:00:47 +0200

    Update Volets.php

--- desktop/php/Volets.php
+++ desktop/php/Volets.php
@@ -46,24 +46,20 @@ $eqLogics = eqLogic::byType('Volets');
 		<legend>{{Mes zones}}</legend>
 		<input class="form-control" placeholder="{{Rechercher}}" style="margin-bottom:4px;" id="in_searchEqlogic" />
 		<div class="eqLogicThumbnailContainer">
-			<?php
+			<?php	
+			if (count($eqLogics) == 0) {
+				echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore de module KNX, cliquez sur Ajouter pour commencer}}</span></center>";
+			} else {
 				foreach ($eqLogics as $eqLogic) {
-					$opacity = '';
-					if ($eqLogic->getIsEnable() != 1) {
-						$opacity = '
-						-webkit-filter: grayscale(100%);
-						-moz-filter: grayscale(100);
-						-o-filter: grayscale(100%);
-						-ms-filter: grayscale(100%);
-						filter: grayscale(100%); opacity: 0.35;';
-					}
+					$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 					echo "<center>";
 					echo '<img src="plugins/Volets/plugin_info/Volets_icon.png" height="105" width="95" />';
 					echo "</center>";
-					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+					echo '<span class="name" style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
 					echo '</div>';
 				}
+			} 
 			?>
 		</div>
 	</div>  

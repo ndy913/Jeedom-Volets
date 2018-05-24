04f68fd10dd647c2a281a449bd2be52c87362d73
Halbout Michael <michaelhalbout@gmail.com>
2018-05-24 16:02:36 +0200

    Update Volets.js

--- desktop/js/Volets.js
+++ desktop/js/Volets.js
@@ -1,6 +1,7 @@
 $("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 $("#table_condition").sortable({axis: "y", cursor: "move", items: ".ConditionGroup", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 $("#table_action").sortable({axis: "y", cursor: "move", items: ".ActionGroup", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+var Template;
 $('.eqLogicAction[data-action=addByTemplate]').on('click', function () {
 	var message = $('<div class="row">')
 		.append($('<div class="col-md-12">')
@@ -23,7 +24,7 @@ $('.eqLogicAction[data-action=addByTemplate]').on('click', function () {
 						.append($('<select class="EqLogicTemplateAttr form-control" data-l1key="template">')
 							   .append($('<option>')
 								.text('{{Séléctionner un template}}')))))
-				   .append($('<label>').text('{{Configurer les adresse de groupe}}'))));
+				   .append($('<label>').text('{{Configurer les parametres}}'))));
 	$.ajax({
 		type: 'POST',            
 		async: false,
@@ -40,7 +41,8 @@ $('.eqLogicAction[data-action=addByTemplate]').on('click', function () {
 				$('#div_alert').showAlert({message: 'Aucun message recu', level: 'error'});
 				return;
 			}
-			$.each(data.result,function(index, value){
+			Template=data.result;
+			$.each(Template,function(index, value){
 				message.find('.EqLogicTemplateAttr[data-l1key=template]')
 					.append($('<option value="'+index+'">')
 				.text(value.name))
@@ -101,6 +103,36 @@ $('.eqLogicAction[data-action=addByTemplate]').on('click', function () {
 		}
 	});
 });
+$('body').on('change','.EqLogicTemplateAttr[data-l1key=template]', function () {
+	//Creation du formulaire du template
+	var form=$(this).closest('form');
+	var Parameters=$('<div class="form-horizontal ParametersTempates">');
+	$.each(Template[$(this).value()],function(index, value){
+		if($.isArray(value)){
+			$.each(Template[$(this).value()],function(index2, value2){
+				if($.isArray(value2)){
+					$.each(Template[$(this).value()],function(index3, value3){
+						Parameters.append(HtmlParameter('data-l1key="'+index+'" data-l2key="'+index2+'" data-l3key="'+index3+'"'));
+					});
+				}
+				else
+					Parameters.append(HtmlParameter('data-l1key="'+index+'" data-l2key="'+index2+'"'));
+			});
+		}
+		else
+			Parameters.append(HtmlParameter('data-l1key="'+index+'"'));
+		
+	});
+	form.find('.ParametersTempates').remove();
+	form.append(Parameters);
+});
+function HtmlParameter(index){
+	return $('<div class="form-group">')
+		.append($('<label class="col-xs-5 control-label" >')
+			.text('{{Nom de votre zone}}'))
+		.append($('<div class="col-xs-7">')
+			.append($('<input class="EqLogicTemplateAttr form-control" '+index+'/>')));
+}
 $('.eqLogicAttr[data-l1key=configuration][data-l2key=heliotrope]').on('change',function(){
 	if($(this).val() != 'Aucun'){
 		$.ajax({
